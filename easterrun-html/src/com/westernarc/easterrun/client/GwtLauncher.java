87861ed7aa054de22f6a5c06f315c6351783609e
westernarc <deepspace30@gmail.com>
2013-02-20 01:13:51 -0500

    First Easterrun Commit, Mockup

---
+++ easterrun-html/src/com/westernarc/easterrun/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package com.westernarc.easterrun.client;
+
+import com.westernarc.easterrun.EasterRun;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new EasterRun();
+	}
+}
